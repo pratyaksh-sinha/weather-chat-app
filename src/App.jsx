9244604278921fed97c1f285a75d12672aa0008d
pratyaksh-sinha <pratyakshajaysinha@gmail.com>
2025-09-05 13:09:38 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-900 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind CSS is Working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
